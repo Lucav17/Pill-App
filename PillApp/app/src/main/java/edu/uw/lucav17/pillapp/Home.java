c3532233bc708f10e8896e85e89881ef778015c9
luca vaccarini <luca vaccarini>
2016-07-31 23:03:23 -0700

    loaded in empty activity

---
+++ PillApp/app/src/main/java/edu/uw/lucav17/pillapp/Home.java
@@ -0,0 +1,13 @@
+package edu.uw.lucav17.pillapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
